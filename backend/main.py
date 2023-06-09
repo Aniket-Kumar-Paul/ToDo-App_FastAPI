a9dee5ddd7f3653bce71993f9640ec566386e81a
Aniket <aniketkumarpaul1@gmail.com>
2023-06-09 11:25:25 +0530

    TODO APP[FASTAPI] - Creating the database and routes functions

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from database import (
+    fetch_one_todo,
+    fetch_all_todos,
+    create_todo,
+    update_todo,
+    remove_todo
+)
+from model import Todo
 
 app = FastAPI()
 
 origins = ['https://localhost:3000']
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
@@ -19,20 +26,33 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo{title}", response_model=Todo)
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f'There is no ToDo item with the title {title}')
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict()) # convert the json todo to dictionary & pass
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong")
 
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title:str, desc:str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f'There is no ToDo item with the title {title}')
 
-@app.delete("/api/todo{id}")
-async def delete_todo(todo):
-    return 1
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo item!"
+    raise HTTPException(404, f'There is no ToDo item with the title {title}')
